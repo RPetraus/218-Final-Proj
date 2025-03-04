1821dde2719a5ccec86b77aed88d968ab9618501
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-03-04 11:43:37 -0500

    Update collisionDetectionSubsystem.h

--- modules/collisionDetectionSubsystem/collisionDetectionSubsystem.h
+++ modules/collisionDetectionSubsystem/collisionDetectionSubsystem.h
@@ -1,1 +1,18 @@
 
+//=====[#include guards - begin]===============================================
+
+#ifndef _COLLISION_DETECTION_SUBSYSTEM_H_
+#define _COLLISION_DETECTION_SUBSYSTEM_H_
+
+//=====[Declaration of public defines]=========================================
+
+//=====[Declaration of public data types]======================================
+
+//=====[Declarations (prototypes) of public functions]=========================
+
+void collisionDetectionSubsystemInit();
+void collisionDetectionSubsystemUpdate();
+
+//=====[#include guards - end]=================================================
+
+#endif // _COLLISION_DETECTION_SUBSYSTEM_H_

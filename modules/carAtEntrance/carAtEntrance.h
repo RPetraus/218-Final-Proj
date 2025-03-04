a631a4343ce39af75d95e973cc14b1154a95a349
brandonmori26 <132004285+brandonmori26@users.noreply.github.com>
2025-03-04 11:28:38 -0500

    Update carAtEntrance.h

--- modules/carAtEntrance/carAtEntrance.h
+++ modules/carAtEntrance/carAtEntrance.h
@@ -1,1 +1,13 @@
 
+//=====[#include guards - begin]===============================================
+
+#ifndef _carAtEntrance_H_
+#define _carAtEntrance_H_
+
+
+
+bool carAtEntrance();
+
+
+//=====[#include guards - end]=================================================
+#endif // _LIGHT_SYSTEM_H_
